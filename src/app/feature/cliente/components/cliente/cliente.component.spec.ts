4e3cbfa0bfa65e4e87dc2e3d50cb898facff5d70
Esthefanía Lemus Preciado - Ceiba Software <esthefania.lemus@ceiba.com.co>
2022-05-20 09:03:28 -0500

    test al servicio cliente

--- src/app/feature/cliente/components/cliente/cliente.component.spec.ts
+++ src/app/feature/cliente/components/cliente/cliente.component.spec.ts
@@ -1,25 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Cliente } from './../../shared/model/cliente';
+import { HttpService } from '@core-service/http.service';
+import { ClienteService } from './../../shared/service/cliente.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import {  TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
+import { HttpResponse } from '@angular/common/http';
 
-import { ClienteComponent } from './cliente.component';
 
 describe('ClienteComponent', () => {
-  let component: ClienteComponent;
-  let fixture: ComponentFixture<ClienteComponent>;
+  let httpMock: HttpTestingController;
+  let service: ClienteService;
+  const apiEndpointCliente = `${environment.endpoint}/cliente`;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ClienteComponent ]
-    })
-    .compileComponents();
-  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ClienteComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ClienteService, HttpService]
+    });
+    httpMock = injector.inject(HttpTestingController);
+    service = TestBed.inject(ClienteService);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const crearCliente: ClienteService = TestBed.inject(ClienteService);
+    expect(crearCliente).toBeTruthy();
+  });
+
+  fit('deberia crear un cliente', () => {
+    const dummyCliente = new Cliente('Sofia','Lemus','42485','s@gmail.com',1);
+    service.guardar(dummyCliente).subscribe((respuesta) => {
+      expect(respuesta).toBeTruthy();
+    });
+    const req = httpMock.expectOne(apiEndpointCliente);
+    expect(req.request.method).toBe('POST');
+    req.event(new HttpResponse<boolean>({body: true}));
   });
 });
